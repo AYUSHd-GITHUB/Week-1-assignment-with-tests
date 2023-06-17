ed5110f73f1e92e58a88ee919265384eeda82897
AYUSHd-GITHUB <115161059+AYUSHd-GITHUB@users.noreply.github.com>
2023-06-17 20:30:50 +0530

    solution to anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,44 @@
 */
 
 function isAnagram(str1, str2) {
+ 
+    var result = [];
+    
+    var wordCheck = function(word, p) {
+      return p.includes(word);
+    };
+  
+    var isAnagram = function(q, t) {
+      if (q.length !== t.length) {
+        return false;
+      }
+  
+      var freq = {};
+      for (var i = 0; i < q.length; i++) {
+        freq[q[i]] = (freq[q[i]] || 0) + 1;
+        freq[t[i]] = (freq[t[i]] || 0) - 1;
+      }
+  
+      for (var char in freq) {
+        if (freq[char] !== 0) {
+          return false;
+        }
+      }
+  
+      return true;
+    };
+    
+    for (var j = 0; j <= s.length ; j++) {
+      var stringSlice = s.slice(j, j + p.length);
+      console.log(stringSlice);
+      if (wordCheck(s[j],p) && isAnagram(stringSlice, p)) {
+        result.push(j);
+      }
+    }
+    
+    return result;
+  };
 
-}
+
 
 module.exports = isAnagram;
